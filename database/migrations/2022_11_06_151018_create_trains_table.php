873bcbb22fa4e087f260d071b9d70dfaf622ef17
FrenzH <franciattom99@gmail.com>
2022-11-06 15:30:51 +0000

    create table

--- database/migrations/2022_11_06_151018_create_trains_table.php
+++ database/migrations/2022_11_06_151018_create_trains_table.php
@@ -15,6 +15,17 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->date('data_di_partenza');
+            $table->date('data_di_arrivo');
+            $table->string('codice_treno', 10)->unique();
+            $table->tinyInteger('numero_carrozze')->unsigned()->default(0);
+            $table->boolean('in_orario')->default(true);
+            $table->tinyInteger('cancellato')->default(0);
             $table->timestamps();
         });
     }
